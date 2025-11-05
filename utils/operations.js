7fe94a64bf3c87f5e71ff83574f9bd10aa820703
AtheerHani <atheer.almomtin@gmail.com>
2025-11-05 11:58:13 +0300

    TODOs done

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,22 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  return numbers.slice(1).reduce((diff, num) => diff - num, numbers[0]);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
+  if (!numbers || numbers.length === 0) return NaN;
+
+  // If any divisor is zero, return NaN to indicate invalid operation
+  const divisors = numbers.slice(1);
+  if (divisors.some((n) => n === 0)) return NaN;
+
+  return divisors.reduce((quotient, num) => quotient / num, numbers[0]);
 }
 
